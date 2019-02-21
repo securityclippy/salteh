98a603981bdbbb5e213cdf5dbf71e0d10313fdba
clippy <securityclippy@securelyinsecure.com>
2019-02-20 16:33:56 -0800

    fixing golang install

--- scripts/install-golang.sh
+++ scripts/install-golang.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 ### thanks https://stefanprodan.com/2016/golang-bash-on-windows-installer/
 
@@ -9,6 +9,42 @@ GFILE="go$GVERSION.linux-amd64.tar.gz"
 
 GOPATH="$HOME/go"
 GOROOT="/usr/local/go"
+
+touch "$HOME/.zshrc"
+{
+    echo '# GoLang'
+    echo 'export PATH=$PATH:/usr/local/go/bin'
+    echo 'export GOPATH=$HOME/go'
+    echo 'export PATH=$PATH:$GOPATH/bin'
+    echo 'export GOROOT=$GOROOT'
+} >> "$HOME/.zshrc"
+echo "GOROOT set to $GOROOT"
+
+mkdir -p "$GOPATH" "$GOPATH/src" "$GOPATH/pkg" "$GOPATH/bin" "$GOPATH/out"
+chmod 777 "$GOPATH" "$GOPATH/src" "$GOPATH/pkg" "$GOPATH/bin" "$GOPATH/out"
+echo "GOPATH set to $GOPATH"
+
+rm -f $TMPDIR/$GFILE
+#source "$HOME/.zshrc"
+
+
+touch "$HOME/.bashrc"
+{
+    echo '# GoLang'
+    echo 'export PATH=$PATH:/usr/local/go/bin'
+    echo 'export GOPATH=$HOME/go'
+    echo 'export PATH=$PATH:$GOPATH/bin'
+    echo 'export GOROOT=$GOROOT'
+} >> "$HOME/.bashrc"
+source "$HOME/.bashrc"
+echo "GOROOT set to $GOROOT"
+
+mkdir -p "$GOPATH" "$GOPATH/src" "$GOPATH/pkg" "$GOPATH/bin" "$GOPATH/out"
+chmod 777 "$GOPATH" "$GOPATH/src" "$GOPATH/pkg" "$GOPATH/bin" "$GOPATH/out"
+echo "GOPATH set to $GOPATH"
+
+rm -f $TMPDIR/$GFILE
+source "$HOME/.bashrc"
 if [ -d $GOROOT ]; then
     echo "Installation directory already exists $GOROOT"
     exit 1
@@ -25,37 +61,3 @@ fi
 
 tar -C "/usr/local" -xzf $TMPDIR/$GFILE
 
-touch "$HOME/.zshrc"
-{
-    echo '# GoLang'
-    echo 'export PATH=$PATH:/usr/local/go/bin'
-    echo 'export GOPATH=$HOME/go'
-    echo 'export PATH=$PATH:$GOPATH/bin'
-} >> "$HOME/.zshrc"
-source "$HOME/.zshrc"
-echo "GOROOT set to $GOROOT"
-
-mkdir -p "$GOPATH" "$GOPATH/src" "$GOPATH/pkg" "$GOPATH/bin" "$GOPATH/out"
-chmod 777 "$GOPATH" "$GOPATH/src" "$GOPATH/pkg" "$GOPATH/bin" "$GOPATH/out"
-echo "GOPATH set to $GOPATH"
-
-rm -f $TMPDIR/$GFILE
-source "$HOME/.zshrc"
-
-
-touch "$HOME/.bashrc"
-{
-    echo '# GoLang'
-    echo 'export PATH=$PATH:/usr/local/go/bin'
-    echo 'export GOPATH=$HOME/go'
-    echo 'export PATH=$PATH:$GOPATH/bin'
-} >> "$HOME/.bashrc"
-source "$HOME/.bashrc"
-echo "GOROOT set to $GOROOT"
-
-mkdir -p "$GOPATH" "$GOPATH/src" "$GOPATH/pkg" "$GOPATH/bin" "$GOPATH/out"
-chmod 777 "$GOPATH" "$GOPATH/src" "$GOPATH/pkg" "$GOPATH/bin" "$GOPATH/out"
-echo "GOPATH set to $GOPATH"
-
-rm -f $TMPDIR/$GFILE
-source "$HOME/.bashrc"
